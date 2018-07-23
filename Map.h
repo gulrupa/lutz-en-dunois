cd3d8cf97be94dd7154078af8c8ae01f5894a0fd
Ethanor <julien.voisin.97@gmail.com>
2018-07-23 09:46:17 +0200

    modification map et pioche

--- Map.h
+++ Map.h
@@ -5,6 +5,13 @@
 #ifndef LUTZ_EN_DUNOIS_MAP_H
 #define LUTZ_EN_DUNOIS_MAP_H
 
+class Map{
+public :
+    void AffichageGrille;
+    bool PoseTuile(int x,int y);
+private :
+    int GrilleDeJeu[140][140]; //on stock le numéro de la tuile à la case correspondant à sa position
+};
 
 
 
--- Pioche.h
+++ Pioche.h
@@ -2,7 +2,20 @@
 // Created by Julien2 on 23/07/2018.
 //
 
+#include "Tuile.h"
+
 #ifndef LUTZ_EN_DUNOIS_PIOCHE_H
 #define LUTZ_EN_DUNOIS_PIOCHE_H
+
+
+
+class Pioche {
+public :
+
+private :
+    int TuileNum[70];
+
+};
+
 
 #endif //LUTZ_EN_DUNOIS_PIOCHE_H
